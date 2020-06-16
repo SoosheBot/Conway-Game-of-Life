1d1ea997752fbd90ac1a993441a9ba8e60c09407
sooshe <sooshe@sooshebot.com>
2020-06-16 12:02:07 -0500

    add modal for rules

--- conway-life-game/src/App.js
+++ conway-life-game/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h4>Nav</h4>
+        {/* <h4>Nav</h4> */}
       </header>
       <Game />
     </div>
